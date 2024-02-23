e23b04e237da65d5b10605cb9ac5f01de13f2df6
Tejas Shah <tejasshah2k19@gmail.com>
2024-02-23 18:49:32 +0530

    insert user

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -1,20 +1,26 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bean.UserBean;
+import com.dao.UserDao;
 
 @RestController
 public class SessionController {
 
+	
+	@Autowired
+	UserDao userDao;
+	
 	@PostMapping("/signup")
 	public UserBean signup(@RequestBody UserBean userBean) {
 		// fn em pw gender
 		
 		//db insert 
-		
+		userDao.addUser(userBean);
 		return userBean;// json
 	}
 }
